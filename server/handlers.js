7201fb10fb1d9c6fc2f791f4b151aeab29a6fcc5
carldumas <dumas.carl@gmail.com>
2020-10-06 22:31:03 -0400

    M6-2: Exercise 2 - Write getSeats function and clean up unecessary code

--- server/handlers.js
+++ server/handlers.js
@@ -1,5 +1,51 @@
 'use strict';
 
-const getSeats = async (req, res) => {};
+const { MongoClient } = require('mongodb');
+const assert = require('assert');
+
+require('dotenv').config();
+const { MONGO_URI } = process.env;
+
+const options = {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+};
+
+const getSeats = async (req, res) => {
+  // creates a new client
+  const client = await MongoClient(MONGO_URI, options);
+
+  try {
+    // connect to the client
+    await client.connect();
+    console.log('Connecting...');
+
+    // connect to the database named 'theater-seats'
+    const db = client.db('theater-seats');
+    console.log('Connected!');
+
+    const seats = await db.collection('seats').find().toArray();
+    const response = {};
+
+    seats.forEach((seat) => {
+      response[seat._id] = seat;
+    });
+
+    // on success, send
+    res.status(200).json({
+      seats: response,
+      numOfRows: 8,
+      seatsPerRow: 12,
+    });
+  } catch (err) {
+    console.log(err.stack);
+    // on failure, send
+    res.status(404).json({ status: 404, err });
+  }
+
+  // close the connection to the database server
+  client.close();
+  console.log('Disconnected!');
+};
 
 module.exports = { getSeats };
--- server/util.js
+++ server/util.js
@@ -10,8 +10,10 @@ const options = {
 
 const assert = require('assert');
 
-// Code that is generating the seats.
-// ----------------------------------
+// *** Below code was taken from routes.js ***
+const NUM_OF_ROWS = 8;
+const SEATS_PER_ROW = 12;
+
 const seats = {};
 const row = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H'];
 for (let r = 0; r < row.length; r++) {
@@ -22,6 +24,40 @@ for (let r = 0; r < row.length; r++) {
     };
   }
 }
+
+// ----------------------------------
+//////// HELPERS
+const getRowName = (rowIndex) => {
+  return String.fromCharCode(65 + rowIndex);
+};
+
+const randomlyBookSeats = (num) => {
+  const bookedSeats = {};
+
+  while (num > 0) {
+    const row = Math.floor(Math.random() * NUM_OF_ROWS);
+    const seat = Math.floor(Math.random() * SEATS_PER_ROW);
+
+    const seatId = `${getRowName(row)}-${seat + 1}`;
+
+    bookedSeats[seatId] = true;
+
+    num--;
+  }
+
+  return bookedSeats;
+};
+
+const bookedSeatsArray = randomlyBookSeats(30);
+
+Object.keys(bookedSeatsArray).forEach((key) => {
+  seats[key].isBooked = true;
+});
+
+Object.values(seats).forEach((seat, index) => {
+  let keysArray = Object.keys(seats);
+  seat._id = keysArray[index];
+});
 
 const batchImport = async () => {
   // declare a variable called `client`, and assign it the `MongoClient()
